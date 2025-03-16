73cade89cb2597080d8ca33f853117d4acf76dce
rayeberechi <faitheberechiraymond@gmail.com>
2025-03-16 17:16:10 +0100

    Fixed case-sensitive import paths for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import NavBar from './Components/navBar';
 import HeroSection from './Components/heroSection'
-import ProductSxn from './components/productSxn';
-import MoreInfo from './components/moreInfo';
-import ReSeller from './components/reSeller';
-import Footer from './components/footer';
-import './components/styles.css';
+import ProductSxn from './Components/productSxn';
+import MoreInfo from './Components/moreInfo';
+import ReSeller from './Components/reSeller';
+import Footer from './Components/footer';
+import './Components/styles.css';
 
 function App() {
   return (
